8fd17657a057e8f8a3b8d063deee1ade407da501
InfiniteX <727160222@qq.com>
2018-06-21 18:30:25 +0800

    开始做LBS

--- app/src/main/java/cn/infinitex/xyy/androidpractice/practice/map/MapActivity.java
+++ app/src/main/java/cn/infinitex/xyy/androidpractice/practice/map/MapActivity.java
@@ -1,15 +1,33 @@
 package cn.infinitex.xyy.androidpractice.practice.map;
 
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.model.BitmapDescriptorFactory;
+import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.maps2d.model.Marker;
+import com.amap.api.maps2d.model.MarkerOptions;
+
 import cn.infinitex.xyy.androidpractice.R;
 
-public class MapActivity extends AppCompatActivity {
-
+public class MapActivity extends AppCompatActivity implements AMap.OnMapLoadedListener {
+    private AMap aMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        MapView mapView = findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        aMap = mapView.getMap();
+        aMap.setOnMapLoadedListener(this);
+    }
+    @Override
+    public void onMapLoaded() {
+
     }
 }
